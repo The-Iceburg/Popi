d9fec0cf36f02fb54273f1e1eb5118cd217746ef
Dockuin <joshuanicholson007@gmail.com>
2022-09-16 20:59:11 +0100

    v0.0.2
    Working to get price to display
    @Ormatist I did it
    Are you proud of me?

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ urlist = str(r.content)
 Bs_data = BeautifulSoup(r.content, "xml")
 b_unique = Bs_data.find_all('loc')
 for i in range(len(b_unique)):
-    b_unique[i] = b_unique[i].replace("<loc>", "")
-    b_unique[i] = b_unique[i].replace("</loc>", "")
+    b_unique[i] = str(b_unique[i]).replace("<loc>", "")
+    b_unique[i] = str(b_unique[i]).replace("</loc>", "")
 
-print(b_unique)
 
-#for i in urllist:
-#    url = urllist[i]
-#    r = rq.get(url, allow_redirects=False)
-#    test2 = str(r.content)
-#    test1 = test2.split('class="coop-u-red-mid">&pound;')
-#    test3 = test1[1].split("<")
-#    print(test3[0])
-#
-#    urlsplit = url.split('/')
-#    urlsplit2 = urlsplit[-1].replace("-", " ")
-#    print(f"The price of {urlsplit2} is £{test3[0]}")
+for i in range(1, len(b_unique)):
+    url = b_unique[i]
+    
+    r = rq.get(url, allow_redirects=False)
+    test2 = str(r.content)
+    test1 = test2.split('&pound;')
+
+    price = test1[1][0]
+
+    urlsplit = url.split('/')
+    urlsplit2 = urlsplit[-1].replace("-", " ")
+    print(f"The price of {urlsplit2} is £{price}")
