c47a5b7e720ae5a4944bff0283b1bd4bdd45a583
Dockuin <joshuanicholson007@gmail.com>
2022-09-17 13:34:30 +0100

    v0.0.3
    Fixed a few things, such as it breaking due to category inclusion
    Also now displays full price rather than just the first digit
    Included a Result File

--- main.py
+++ main.py
@@ -12,15 +12,22 @@ for i in range(len(b_unique)):
     b_unique[i] = str(b_unique[i]).replace("</loc>", "")
 
 
-for i in range(1, len(b_unique)):
+
+for i in range(0, len(b_unique)):
     url = b_unique[i]
     
     r = rq.get(url, allow_redirects=False)
     test2 = str(r.content)
+
     test1 = test2.split('&pound;')
+    if len(test1) != 2:
+        continue
+    else:
+        price = test1[1].partition("\\")
 
-    price = test1[1][0]
-
-    urlsplit = url.split('/')
-    urlsplit2 = urlsplit[-1].replace("-", " ")
-    print(f"The price of {urlsplit2} is £{price}")
+        urlsplit = url.split('/')
+        urlsplit2 = urlsplit[-1].replace("-", " ")
+        # print(f"The price of {urlsplit2} is £{price[0]}")
+        with open("result.txt","a") as f:
+            f.write(f"The price of {urlsplit2} is £{price[0]}\n")
+    
